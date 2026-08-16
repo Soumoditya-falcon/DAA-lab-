3469281be3fdca4052c1a0c8433044ac05be6ea1
Soumoditya-falcon <dasanjali987@gmail.com>
2026-08-16 23:39:29 +0530

    Refactor matrix operations and replace I/O methods
    Multiply special-pattern square matrices using D&C approach: Two n×nmatrices are provided to you, where n = 2k for some natural number k. Each matrix has the recursive structure described as: when divided into four equal-sized blocks, two diagonal blocks areidentical, and two off-diagonal blocks are identical, i.e. the structure of the matrices would be: M= M1 M2 M2 M1 . Each block has a recursive structure that goes down to single integer elements. Give a divide-and-conquer approach-based O(n2) algorithm for multiplying two such matrices and validate the complexity of your algorithm

--- lab 3/q5.cpp
+++ lab 3/q5.cpp
@@ -1,42 +1,126 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 
-class Student
+void add(int n, int A[n][n], int B[n][n], int C[n][n])
 {
-    int rollNumber;
-    string name;
-    float marks;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            C[i][j] = A[i][j] + B[i][j];
+}
 
-public:
-    void accept()
+void sub(int n, int A[n][n], int B[n][n], int C[n][n])
+{
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            C[i][j] = A[i][j] - B[i][j];
+}
+
+void normalMultiply(int n, int A[n][n], int B[n][n], int C[n][n])
+{
+    for (int i = 0; i < n; i++)
     {
-        cout << "Enter Roll Number: ";
-        cin >> rollNumber;
+        for (int j = 0; j < n; j++)
+        {
+            C[i][j] = 0;
 
-        cout << "Enter Name: ";
-        cin >> name;
+            for (int k = 0; k < n; k++)
+                C[i][j] += A[i][k] * B[k][j];
+        }
+    }
+}
 
-        cout << "Enter Marks: ";
-        cin >> marks;
+void specialMultiply(int n, int A[n][n], int B[n][n], int C[n][n])
+{
+    if (n == 1)
+    {
+        C[0][0] = A[0][0] * B[0][0];
+        return;
     }
 
-    void display()
+    int k = n / 2;
+
+    int A1[k][k], A2[k][k];
+    int B1[k][k], B2[k][k];
+
+    int X[k][k], Y[k][k];
+    int P[k][k], Q[k][k];
+
+    int C1[k][k], C2[k][k];
+
+    for (int i = 0; i < k; i++)
     {
-        cout << "\nStudent Details" << endl;
-        cout << "Roll Number: " << rollNumber << endl;
-        cout << "Name: " << name << endl;
-        cout << "Marks: " << marks << endl;
+        for (int j = 0; j < k; j++)
+        {
+            A1[i][j] = A[i][j];
+            A2[i][j] = A[i][j + k];
+
+            B1[i][j] = B[i][j];
+            B2[i][j] = B[i][j + k];
+        }
     }
-};
+
+    add(k, A1, A2, X);
+    add(k, B1, B2, Y);
+
+    specialMultiply(k, X, Y, P);
+
+    sub(k, A1, A2, X);
+    sub(k, B1, B2, Y);
+
+    specialMultiply(k, X, Y, Q);
+
+    for (int i = 0; i < k; i++)
+    {
+        for (int j = 0; j < k; j++)
+        {
+            C1[i][j] = (P[i][j] + Q[i][j]) / 2;
+            C2[i][j] = (P[i][j] - Q[i][j]) / 2;
+        }
+    }
+
+    for (int i = 0; i < k; i++)
+    {
+        for (int j = 0; j < k; j++)
+        {
+            C[i][j] = C1[i][j];
+            C[i][j + k] = C2[i][j];
+            C[i + k][j] = C2[i][j];
+            C[i + k][j + k] = C1[i][j];
+        }
+    }
+}
 
 int main()
 {
-    Student *s = new Student;
+    int n;
 
-    s->accept();
-    s->display();
+    printf("Enter n (power of 2): ");
+    scanf("%d", &n);
 
-    delete s;
+    int A[n][n], B[n][n], C[n][n];
+
+    printf("Enter Matrix A:\n");
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            scanf("%d", &A[i][j]);
+
+    printf("Enter Matrix B:\n");
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            scanf("%d", &B[i][j]);
+
+    specialMultiply(n, A, B, C);
+
+    printf("Result Matrix:\n");
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+            printf("%d ", C[i][j]);
+
+        printf("\n");
+    }
 
     return 0;
 }
