edbb114ae3e31666c420fd6b606b94bc2843bf29
Soumoditya-falcon <dasanjali987@gmail.com>
2026-08-16 12:50:42 +0530

    Add Student class with accept and display methods

---
+++ lab 3/q5.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class Student
+{
+    int rollNumber;
+    string name;
+    float marks;
+
+public:
+    void accept()
+    {
+        cout << "Enter Roll Number: ";
+        cin >> rollNumber;
+
+        cout << "Enter Name: ";
+        cin >> name;
+
+        cout << "Enter Marks: ";
+        cin >> marks;
+    }
+
+    void display()
+    {
+        cout << "\nStudent Details" << endl;
+        cout << "Roll Number: " << rollNumber << endl;
+        cout << "Name: " << name << endl;
+        cout << "Marks: " << marks << endl;
+    }
+};
+
+int main()
+{
+    Student *s = new Student;
+
+    s->accept();
+    s->display();
+
+    delete s;
+
+    return 0;
+}
